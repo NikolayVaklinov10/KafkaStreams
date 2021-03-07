b2866f3cfd245760e4beede8330d61301a84db1a
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-07 14:00:07 +0000

    more assertions

--- src/test/java/com/kafka/bankApp/BankTransactionsProducerTest.java
+++ src/test/java/com/kafka/bankApp/BankTransactionsProducerTest.java
@@ -1,5 +1,8 @@
 package com.kafka.bankApp;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.junit.Test;
 
@@ -14,6 +17,17 @@ public class BankTransactionsProducerTest {
         String value = record.value();
 
         assertEquals(key, "john");
+
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode node = mapper.readTree(value);
+            assertEquals(node.get("name").asText(), "john");
+            assertTrue( node.get("amount").asInt() < 100);
+//            assertEquals(node.get("time").asText(), "john");
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
         System.out.println(value);
 
 
