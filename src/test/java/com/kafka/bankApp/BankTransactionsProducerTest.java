4d303a81b4e575db46c1b5dd52443bf4d579d9b8
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-07 13:20:36 +0000

    testing the bank transaction code with unit test

---
+++ src/test/java/com/kafka/bankApp/BankTransactionsProducerTest.java
@@ -0,0 +1,21 @@
+package com.kafka.bankApp;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankTransactionsProducerTest {
+
+    @Test
+    public void newRandomTransactionTest(){
+        ProducerRecord<String, String> record = BankTransactionsProducer.newRandomTransaction("john");
+        String key = record.key();
+        String value = record.value();
+
+        assertEquals(key, "john");
+
+
+    }
+
+}
