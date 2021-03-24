34deacbedc9f09e807ca20f3d5953d4bda557fae
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-24 15:48:03 +0000

    the User profile class needed to create artificial users and data

---
+++ src/main/java/com/kafka/stockstats/model/UserProfile.java
@@ -0,0 +1,43 @@
+package com.kafka.stockstats.model;
+
+public class UserProfile {
+    int userID;
+    String userName;
+    String zipcode;
+    String[] interests;
+
+    public UserProfile(int userID, String userName, String zipcode, String[] interests) {
+        this.userID = userID;
+        this.userName = userName;
+        this.zipcode = zipcode;
+        this.interests = interests;
+    }
+
+    public UserProfile userProfile(String zipcode, String[] interests) {
+        this.zipcode = zipcode;
+        this.interests = interests;
+        return this;
+    }
+
+    public UserProfile update(String zipcode, String[] interests) {
+        this.zipcode = zipcode;
+        this.interests = interests;
+        return this;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public String[] getInterests() {
+        return interests;
+    }
+}
