827e4dc5acb3df5770c88f83b350704ed78d641d
NikolayVaklinov10 <nikolayvaklinov@yahoo.com>
2021-03-26 21:03:30 +0000

    search class for adding it in the online app

---
+++ src/main/java/com/kafka/clickstream/model/Search.java
@@ -0,0 +1,19 @@
+package com.kafka.clickstream.model;
+
+public class Search {
+    int userID;
+    String searchTerms;
+
+    public Search(int userID, String searchTerms) {
+        this.userID = userID;
+        this.searchTerms = searchTerms;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getSearchTerms() {
+        return searchTerms;
+    }
+}
